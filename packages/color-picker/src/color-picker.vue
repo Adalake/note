690510f2329030f8424cb8acd0e11d235b39b371
DESKTOP-28LMEQ7\admin <gaomintv@sohu.com>
2021-05-27 14:50:39 +0800

    从0开始搭建组件库，并推送到npm  【finished】

---
+++ packages/color-picker/src/color-picker.vue
@@ -0,0 +1,19 @@
+<template>
+	<colorPicker v-model="color" v-on:change="headleChangeColor"></colorPicker>
+</template>
+
+<script>
+export default {
+    name: 'color-picker',
+	data () {
+		return {
+			color: '#ff0000'
+		}
+	},
+	methods: {
+		headleChangeColor () {
+			console.log('颜色改变')
+		}
+	}
+}
+</script>
